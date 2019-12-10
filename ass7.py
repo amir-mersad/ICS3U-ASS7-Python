a0625b27dc88aa485a060d696df4059626c3d4d4
Ubuntu <ubuntu@ip-172-31-18-12.ec2.internal>
2019-12-10 22:19:35 +0000

    program added

---
+++ ass7.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Created by: Amir Mersad
+# Created on: November 2019
+# This program rounds numbers
+
+
+def printing(input_list):
+    # Process
+    line = ""
+    result = ""
+    for word in input_list:
+        line = f"* {word} *"
+        if word == input_list[-1]:
+            result += line
+        else:
+            result += line + "\n"
+    return result
+
+
+def main():
+    # This function gets the input and passes it to another function
+    input_list = []
+
+    # Input and part of Process
+    while True:
+        word = input('Enter the word:\t (type "stop!" when done)')
+        if word == "stop!":
+            break
+        elif word == "":
+            pass
+        else:
+            input_list.append(word)
+
+    # Passing to another funtion
+    result = printing(input_list)
+
+    # Output
+    print("*********")
+    print(result)
+    print("*********")
+
+
+if __name__ == "__main__":
+    main()
